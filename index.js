fb3887648560045874e8514f92fbfe540b84f23f
josecaos <jazzvoon@gmail.com>
2019-09-07 14:04:58 -0500

    Load + Play

--- index.js
+++ index.js
@@ -1,7 +1,49 @@
+
+
 (
   function() {
 
-    alert("Hello World")
+    const AudioContext = window.AudioContext || window.webkitAudioContext
+    const audioContext = new AudioContext()
+    const audioElement = new Audio()
+    const playButton = document.querySelector('.toggle-play');
+    const track = audioContext.createMediaElementSource(audioElement)//carga elemento html
+    let url = "./josecaos-j71g1gj7-soundcloud.edit.mp3"
+
+    audioElement.src = url
+    // audioElement.type = "audio/mpeg"
+    audioElement.crossOrigin = "anonymous";
+    audioElement.controls = false
+    audioElement.autoplay = false
+    audioElement.loop = true
+    track.connect(audioContext.destination)
+
+    console.log(audioElement)
+
+    playButton.addEventListener('click', function() {
+
+      // check if context is in suspended state (autoplay policy)
+      if (audioContext.state === 'suspended') {
+        audioContext.resume();
+      }
+
+      if (this.dataset.playing === 'false') {
+        // agrega el archivo de audioContext
+        audioElement.setAttribute('src',url)
+        audioElement.play();
+        this.dataset.playing = 'true';
+      } else if (this.dataset.playing === 'true') {
+        audioElement.pause();
+        this.dataset.playing = 'false';
+      }
+
+    }, false)
+
+    // termina play callback
+    audioElement.addEventListener('ended', (e) => {
+      playButton.dataset.playing = 'false'
+      console.log("Fin play: " + e);
+    }, false)
 
   }()
 )
---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+<head>
+  <meta charset="utf-8">
+  <title>Audio player</title>
+</head>
+
+<body>
+
+  <!-- tarjeta player -->
+  <section id="tarjeta-player">
+    <div class="wrapper">
+
+      <!-- <audio class="player" type="audio/ogg"></audio> -->
+      <!-- <audio class="player" src="/home/josecaos/Documents/CCD-agenda.digital/proyectos/inali/inali.repos/inali-frontend/src/componentes/Audio/AudioAssets/josecaos-j71g1gj7-soundcloud.edit.ogg" type="audio/mpeg"></audio> -->
+      <button class="toggle-play" data-playing="false" role="switch" aria-checked="false">
+        <span>Play/Pause</span>
+      </button>
+
+</div>
+</section>
+<!--  -->
+
+<script src="index.js" type="text/javascript"></script>
+</body>
+
+</html>
